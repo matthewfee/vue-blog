a36e0ee4dbdbb2ae255bfc4e270ee9460c8c561c
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-07 10:02:36 +0200

    initialized project

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -32,7 +32,7 @@ export default {
     },
     {
       name: 'Github Repo',
-      link: 'https://github.com/danielkellyio/awake-template',
+      link: 'https://github.com/matthewfee/vue-blog',
       target: '_blank'
     },
     {
